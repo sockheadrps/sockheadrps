25e250c94e3e904008ace25664ffe8e1929c4e4e
sockheadrps <r.p.skiles@gmail.com>
2024-09-24 00:27:32 -0400

    generate PRs section

--- update_config.py
+++ update_config.py
@@ -8,11 +8,13 @@ config.read('config.ini')
 # Get the environment variable for the GitHub username
 github_user_name = 'sockheadrps'
 gif_frame_duration = '6000'
+generate_merged_prs = 'true'
 
 
 # Update the github_user_name in the config file
 config.set('Settings', 'github_user_name', github_user_name)
 config.set('Settings', 'gif_frame_duration', gif_frame_duration)
+config.set('Readme', 'generate_merged_prs', generate_merged_prs)
 
 # Write the changes back to the config.ini file
 with open('config.ini', 'w') as configfile:
